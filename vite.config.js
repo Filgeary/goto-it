79c424274527e41b529222fa2b1a10b702178dfb
Georgiy Filippov <g.filippov777@gmail.com>
2024-06-06 06:23:03 +0500

    chore: preload images in `index.html` on build time

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+// @ts-check
 import react from '@vitejs/plugin-react'
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    // preload all images (webp, svg)
+    UnpluginInjectPreload({
+      files: [
+        {
+          entryMatch: /[a-zA-Z0-9_-]*\.(webp|svg)$/,
+        },
+      ],
+    }),
+  ],
 })
